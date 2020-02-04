65402c1f04a4b2b431025c346b921a18b541e041
Carlos <carlos.delgado@hotmail.it>
2020-02-04 17:45:11 +0100

    creating 'funds in atm' test and pincode argument

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,29 +1,37 @@
 class Atm
 
     attr_accessor :funds
+
     def initialize
         @funds = 1000
-      end
-
-    def withdraw(amount, account) 
-        case
-        when insufficcient_funds_in_account?(amount, account)
-            return
-  else
-    perform_transaction(amount, account)
-      end
     end
 
-private
+    def withdraw(amount, pin_code, account) 
+        case
+        when insufficcient_funds_in_account?(amount, account)
+            { status: false, message: 'insufficient funds', date: Date.today }
+        when insufficient_funds_in_atm?(amount)
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
+            
+        else
+            perform_transaction(amount, account)
+        end
+    end
 
-def insufficcient_funds_in_account?(amount, account)
-amount > account.balance 
-end
+    private
 
-def perform_transaction(amount, account)
-  @funds -= amount 
-  account.balance = account.balance - amount
-  { status: true, message: 'success', date: Date.today, amount: amount }
-end
+    def insufficient_funds_in_atm?(amount)
+        @funds < amount
+       end
+
+    def insufficcient_funds_in_account?(amount, account)
+        amount > account.balance 
+    end
+
+    def perform_transaction(amount, account)
+        @funds -= amount 
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount: amount }
+    end
 
 end
