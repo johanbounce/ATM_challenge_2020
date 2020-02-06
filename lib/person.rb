598ae90b9cd20b3686824eb6dc05547d8f0d5c60
Carlos <carlos.delgado@hotmail.it>
2020-02-06 16:52:46 +0100

    expect to have account attribute test

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,14 @@
 class Person
 
-    attr_accessor :name
+    attr_accessor :name, :cash, :account
 
   def initialize(attrs = {})
     @name = set_name(attrs[:name])   
+    @cash = 0
+    @account = nil
   end
+    
+ 
 
   private
 
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,4 +1,5 @@
 require './lib/person.rb'
+require './lib/account.rb'
 require './lib/atm.rb'
 
 describe Person do
@@ -13,6 +14,24 @@ describe Person do
     expect { described_class.new }.to raise_error 'A name is required'
   end
 
+  it 'is expected to have a :cash attribute with the value of 0 on initialize' do
+    expect(subject.cash).to eq 0
+  end
+
+  it 'is expected to have a :account attribute' do
+    expect(subject.account).to be nil
+  end
+
+  describe 'can create an Account' do
+    # As a Person,
+    # to be able to use banking services to manage my funds,
+    # I would like to be able to create a bank account
+    before { subject.create_account }
+    it 'of Account class ' do
+      expect(subject.account).to be_an_instance_of Account
+    end
+
+
 end
 
-
+end
