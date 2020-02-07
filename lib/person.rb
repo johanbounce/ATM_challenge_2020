5e6f8124c9fdee8535339aedef93a41bc5f66e37
bounce <johan@waytogo.se>
2020-02-07 11:38:30 +0100

    test for deposit requiring account

--- lib/person.rb
+++ lib/person.rb
@@ -6,27 +6,25 @@ class Person
     @name = set_name(attrs[:name])   
     @cash = 0
     @account = nil
-    
-    
   end
-    
+
   # we need to review this method- we don+t think it serves the purpose even thought the test passes
- def create_account
+  def create_account
     @account = Account.new(owner: name)
- end  
+  end  
 
+  #   def deposit (amount) 
+  #     @cash += amount 
+  #    @account = nil ? missing_account : deposit(amount)
+  # end
 
- def deposit (amount) 
-  @cash =   amount 
-        
- end
  
-
-
   private
 
-  
 
+  def missing_account
+    raise "An account is required to manage your funds"
+  end  
 
   def set_name(obj)
     obj == nil ? missing_name : obj
@@ -35,4 +33,6 @@ class Person
   def missing_name
     raise "A name is required"
   end
+
+  
 end    
