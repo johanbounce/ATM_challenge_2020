6d84bd9231bbf2fd9f0ce759ef9488f3511de9f3
Carlos <carlos.delgado@hotmail.it>
2020-02-07 13:16:45 +0100

    integrating all parts test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -39,13 +39,24 @@ describe Person do
 
   describe 'can manage funds if an account been created' do
     let(:atm) { Atm.new }
-    # As a Person with a Bank Account,
-    # in order to be able to put my funds in the account ,
-    # i would like to be able to make a deposit
+  
     before { subject.create_account }
     it 'can deposit funds' do
       expect(subject.deposit(100)).to be_truthy
     end
+
+    it 'funds are added to the account balance - deducted from cash' do
+      subject.cash = 100
+      subject.deposit(100)
+      expect(subject.account.balance).to be 100
+      expect(subject.cash).to be 0
+    end
+
+    it 'can withdraw funds' do
+      command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm) }
+      expect(command.call).to be_truthy
+    end
+    
   end
   describe 'can not manage funds if no account been created' do
     # As a Person without a Bank Account,
